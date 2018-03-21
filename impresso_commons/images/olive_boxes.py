d1d2a060dce99f212737b8a37b2ef27dbea9578f
maudehrmann <maud.ehrmann@gmail.com>
2018-03-21 16:45:26 +0100

    update logging, docstring and strategy params

--- impresso_commons/images/olive_boxes.py
+++ impresso_commons/images/olive_boxes.py
@@ -3,26 +3,31 @@
 Functions to support re-computation of Olive box coordinates.
 """
 
-__author__ = "maudehrmann"
-
+import logging
 import cv2 as cv
 from bs4 import BeautifulSoup
 import os
 import zipfile
 import numpy as np
 
-import utils
+from impresso_commons.images import utils
+
+__author__ = "maudehrmann"
+
+
+logger = logging.getLogger(__name__)
 
 
 def get_iiif_url(page_id, box):
     """ Returns impresso iiif url given a page id and a box
 
-    :param page_id: impresso page id, e.g. EXP-1930-06-10-a-p0001
-    :type str
-    :param box: iiif box (x, y, w, h)
-    :type str (4 coordinate values blank separated)
+    :param page_id  : impresso page id, e.g. EXP-1930-06-10-a-p0001
+    :type page_id   : str
+    :param box      : iiif box (x, y, w, h)
+    :type box       : str (4 coordinate values blank separated)
+    :return         : iiif url of the box
+    :rtype          : str
     """
-    # TODO: replace spaces in box
     base = "http://dhlabsrv17.epfl.ch/iiif_impresso"
     suffix = "full/0/default.jpg"
     return os.path.join(base, page_id, box.replace(" ", ","), suffix)
@@ -31,10 +36,13 @@ def get_iiif_url(page_id, box):
 def compute_scale_factor(img_source_path, img_dest_path):
     """ Computes x scale factor bewteen 2 images
 
-    :param img_source_path: the source image
-    :type img_source_path: full path to the image
-    :param img_dest_path: the destination image
-    :type img_dest_path: full path to the image
+    :param img_source_path  : the source image
+    :type img_source_path   : full path to the image
+    :param img_dest_path    : the destination image
+    :type img_dest_path     : full path to the image
+    :return                 : scale factor
+    :rtype:                 : float
+
     """
 
     img_s = cv.imread(img_source_path, 0)
@@ -48,16 +56,17 @@ def compute_box(scale_factor, input_box):
     """
     Compute IIIF box coordinates of input_box relative to scale_factor.
 
-    :param scale_factor: ratio between 2 images with different dimensions
-    :type scale_factor: float
-    :param input_box: string with 4 values separated by spaces
-    :type input_box: str
-    :return: str
+    :param scale_factor : ratio between 2 images with different dimensions
+    :type scale_factor  : float
+    :param input_box    : string with 4 values separated by spaces
+    :type input_box     : str
+    :return             : new box coordinates
+    :rtype:             : str
     """
     try:
         elems = input_box.split(" ")
     except ValueError:
-        print(f'Invalid box format: {input_box}')
+        logger.info(f'Invalid box format: {input_box}')
         return
 
     x = round(int(elems[0]) * scale_factor)
@@ -71,14 +80,15 @@ def convert_box(box):
     """
     Convert a box with [x y x y] coordinates to [x y w h]
 
-    :param box: box with 4 coordinates, x and y upper left and lower right
-    :type box:
-    :return: str: box with 4 coordinates, x and y upper left, width and height
+    :param box  : box with 4 coordinates, x and y upper left and lower right
+    :type box   : str
+    :return     : box with 4 coordinates, x and y upper left, width and height
+    :rtype:     : str
     """
     try:
         elems = box.split(" ")
     except ValueError:
-        print(f'Invalid box format: {input_box}')
+        logger.info(f'Invalid box format: {input_box}')
         return
 
     w = (elems[2] - elems[0])
@@ -86,9 +96,21 @@ def convert_box(box):
     return elems[0].join(elems[1], w, h)
 
 
-def get_scale_factor(archive, page_xml, box_strategy, img_source_name, img_dest_name):
+def get_scale_factor(issue_dir_path, archive, page_xml, box_strategy, img_source_name):
     """
     Returns the scale factor in Olive context, given a strategy to choose the source image.
+
+    :param issue_dir_path   : the path of the issue
+    :type  issue_dir_path   : str
+    :param archive          : the zip archive
+    :type  archive          : zipfile.ZipFile
+    :param page_xml         : the xml handler of the page
+    :type page_xml          : bytes
+    :param box_strategy     : the box strategy such as found in the info.txt from jp2 folder
+    :type box_strategy      : str
+    :param img_source_name  : as found in the info.txt from jp2 folder
+    :return:                : the hopefully correct scale factor
+    :rtype                  : float
 
     Olive box coordinates were computed according to an image source which we have to identify.
     Image format coverage is different from issue to issue, and we have to devise strategies.
@@ -117,68 +139,62 @@ def get_scale_factor(archive, page_xml, box_strategy, img_source_name, img_dest_
     Same as Case 3, scale factor of 1.
     Here we do an additional check to see if the page_width|height are the same as the image ones.
     (there is only one image and things should fit, not like in case 2)
-
-    :param archive:
-    :param page_xml:
-    :param box_strategy:
-    :param img_source_name:
-    :param img_dest_name:
-    :return:
     """
 
     page_soup = BeautifulSoup(page_xml, "lxml")
     page_root = page_soup.find("xmd-page")
-    source_res = 0
-    dest_res = 0
+    page_number = page_root.meta["page_no"]
 
-    if box_strategy == utils.BoxStrategy.tif:
+    if box_strategy == utils.BoxStrategy.tif.name:
         for f in page_root.datafiles.find_all('files'):
             if f['type'] == 'PAGE_IMG' and f['present'] == "1":
                 source_res = f['xresolution_dpi']
                 dest_res = page_root.meta['images_resolution']
                 break
-        if source_res != 0 and dest_res != 0:
+        if source_res and dest_res:
             return int(source_res) / int(dest_res)
         else:
-            print(f"Impossible to get resolution in case [tif]")  # need to have a pointer
+            logger.info("Impossible to get resolution in case: tif"
+                        " in {issue_dir_path}, page {page_number}")
             return None
 
-    elif box_strategy == utils.BoxStrategy.png_highest:
+    elif box_strategy == utils.BoxStrategy.png_highest.name:
         if "_" not in img_source_name:
-            print(f"Not valid png filename {img_source_name}")
+            logger.info(f"Not valid png filename {img_source_name}")
             return None
 
         png_res = os.path.splitext(img_source_name)[0].split("_", 1)[-1]
         olive_res = page_root.meta['images_resolution']
         if png_res == olive_res:
-            return 1
+            return 1.0
         else:
-            print(f"Incompatible resolutions between highest png and olive indications")  # need to have a pointer
+            logger.info("Incompatible resolutions between highest png and olive indications"
+                        " in {issue_dir_path}, page {page_number}")
             return None
 
-    elif box_strategy == utils.BoxStrategy.png_uniq:
-        # TODO
-        print("not ready")
+    elif box_strategy == utils.BoxStrategy.png_uniq.name:
+        # TODO if needed
+        logger.info("Finally found a case of png_uniq, which is not ready yet")
 
-    elif box_strategy == utils.BoxStrategy.jpg_uniq:
-        # get the x dimension of the unique jpg, from which jp2 was acquired
+    elif box_strategy == utils.BoxStrategy.jpg_uniq.name:
+        # get the x dimension of the unique jpg (from which jp2 was acquired)
+        # and compare with olive's one
         img_data = archive.read(img_source_name)
         img = cv.imdecode(np.frombuffer(img_data, np.uint8), 1)
         jpg_x_dim = img.shape[1]
 
-        # get olive's one
-        img_s = cv.imread(img_source_name, 0)
         olive_x_dim = page_root.meta['page_width']
 
-        # check if equals
         if jpg_x_dim == int(olive_x_dim):
-            return 1
+            return 1.0
         else:
-            print(f"Incompatible resolutions between uniq jpg and olive indications")  # need to have a pointer
+            logger.info("Incompatible resolutions between uniq jpg and olive indications"
+                        " in {issue_dir_path}, page {page_number}.")
             return None
 
 
 def test():
+    """ a testing function """
     base_dir = "/Users/maudehrmann/work/work-projects/impresso/code/impresso-image-acquisition/sample-data"
 
     # tifs
@@ -186,10 +202,10 @@ def test():
     archive = os.path.join(base_dir, "TEST/1900/01/10/Document.zip")
     working_archive = zipfile.ZipFile(archive)
     data = working_archive.read("1/Pg001.xml")
-    sf = get_scale_factor(working_archive, data, utils.BoxStrategy.tif, None, None)
+    sf = get_scale_factor("fictious path", working_archive, data, utils.BoxStrategy.tif.name, None)
     newbox = compute_box(sf, box)
     iiif = get_iiif_url("GDL-1900-01-10-a-p0001", newbox)
-    print("\nCASE: jpj uniq - word 'hambourg'")
+    print("\nCASE: jpj uniq - word 'Centenaire'")
     print(f"Scale factor: {sf}")
     print(f"Newbox: {newbox}")
     print(f"IIIF: {iiif}")
@@ -199,10 +215,10 @@ def test():
     archive = os.path.join(base_dir, "TEST/1900/01/11/Document.zip")
     working_archive = zipfile.ZipFile(archive)
     data = working_archive.read("1/Pg001.xml")
-    sf = get_scale_factor(working_archive, data, utils.BoxStrategy.png_highest, "Img/Pg001_180.png", None)
+    sf = get_scale_factor("fictious path", working_archive, data, utils.BoxStrategy.png_highest.name, "Img/Pg001_180.png")
+    print(sf)
     newbox = compute_box(sf, box)
     iiif = get_iiif_url("EXP-1889-07-01-a-p0001", newbox)
-    print(sf)
     print("\nCASE: png_highest - word 'NEUCHATEL'")
     print(f"Scale factor: {sf}")
     print(f"Newbox: {newbox}")
@@ -213,7 +229,7 @@ def test():
     archive = os.path.join(base_dir, "TEST/1900/01/12/Document.zip")
     working_archive = zipfile.ZipFile(archive)
     page_data = working_archive.read("1/Pg001.xml")
-    sf = get_scale_factor(working_archive, page_data, utils.BoxStrategy.jpg_uniq, "1/Img/Pg001.jpg", None)
+    sf = get_scale_factor("fictious path", working_archive, page_data, utils.BoxStrategy.jpg_uniq.name, "1/Img/Pg001.jpg")
     newbox = compute_box(sf, box)
     iiif = get_iiif_url("LCE-1868-08-02-a-p0001", newbox)
     print("\nCASE: jpj uniq - word 'hambourg'")
