bf2dece6565af2667656670996d4076913c23a2d
mromanello <matteo.romanello@gmail.com>
2018-05-22 17:15:51 +0200

    rewrote the get_bucket: now supports versioning, and creation of bucket if not found

--- impresso_commons/utils/s3.py
+++ impresso_commons/utils/s3.py
@@ -4,10 +4,87 @@ import os
 import logging
 import json
 import boto
+import boto3
 import boto.s3.connection
 from smart_open import s3_iter_bucket
 
 logger = logging.getLogger(__name__)
+
+
+def get_s3_client(url='https://os.zhdk.cloud.switch.ch/'):
+    """Create a client connected to impresso's S3 drive.
+    Assumes that two environment variables are set: `SE_ACCESS_KEY` and
+        `SE_SECRET_KEY`.
+
+    :param url: the s3 endpoint's URL
+    :type url: string
+    """
+    try:
+        access_key = os.environ["SE_ACCESS_KEY"]
+    except Exception:
+        raise
+
+    try:
+        secret_key = os.environ["SE_SECRET_KEY"]
+    except Exception:
+        raise
+
+    return boto3.resource(
+        's3',
+        aws_secret_access_key=secret_key,
+        aws_access_key_id=access_key,
+        endpoint_url=url
+    )
+
+
+def get_bucket(name, create=False, versioning=True):
+    """Create an s3 connection and returns the requested bucket.
+
+    It is possible to ask for creating a new bucket
+    with the specified name (in case it does not exist), and (optionally)
+    to turn on the versioning on the newly created bucket.
+
+    >>> b = get_bucket('testb', create=False)
+    >>> b = get_bucket('testb', create=True)
+    >>> b = get_bucket('testb', create=True, versioning=False)
+
+    :param name: the bucket's name
+    :type name: string
+    :param create: creates the bucket if not yet existing
+    :type create: boolean
+    :param versioning: whether the new bucket should be versioned
+    :type versioning: boolean
+    :return: an s3 bucket
+    :rtype: `boto.s3.bucket.Bucket`
+
+    .. TODO:: avoid import both `boto` and `boto3`
+    """
+    conn = get_s3_connection()
+    # try to fetch the specified bucket -- may return an empty list
+    bucket = [b for b in conn.get_all_buckets() if b.name == name]
+
+    try:
+        assert len(bucket) > 0
+        return bucket[0]
+
+    # bucket not found
+    except AssertionError:
+        if create:
+            bucket = conn.create_bucket(name)
+            print(f'New bucket {name} was created')
+        else:
+            print(f'Bucket {name} not found')
+            return None
+
+    # enable versioning
+    if versioning:
+        client = get_s3_client()
+        versioning = client.BucketVersioning(name)
+        versioning.enable()
+
+    print(bucket.get_versioning_status())
+
+    return bucket
 
 
 def get_s3_connection(host="os.zhdk.cloud.switch.ch"):
