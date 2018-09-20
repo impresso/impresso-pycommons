ce0684cead2bbd2047049dbdb9925c75e1a2d85c
maudehrmann <maud.ehrmann@gmail.com>
2018-09-20 10:57:56 +0200

    added project name

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('../'))
 
 project = 'Impresso PyCommons'
 copyright = '2018, impresso team'
-author = 'impresso team'
+author = 'impresso team - Media Monitoring of the Past - SNSF CR-SII5_173719.'
 
 # The short X.Y version
 version = ''
