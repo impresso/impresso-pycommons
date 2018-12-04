a8c9dcc9a0a30bdb4cb34df78c964d51a4b78d31
mromanello <matteo.romanello@gmail.com>
2018-12-04 12:56:27 +0100

    updated names of s3 buckets in tests

--- tests/test_path_s3.py
+++ tests/test_path_s3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import pytest
-from impresso_commons.path.path_s3 import impresso_iter_bucket, s3_filter_archives, s3_iter_bucket
+from impresso_commons.path.path_s3 import (impresso_iter_bucket,
+                                           s3_filter_archives, s3_iter_bucket)
 from impresso_commons.utils.s3 import get_bucket
 
 
@@ -12,7 +12,7 @@ def test_impresso_iter_bucket():
     }
 
     iter_items = impresso_iter_bucket(
-        bucket_name="canonical-rebuilt-versioned",
+        bucket_name="original-canonical-data",
         item_type="content_item",
         # prefix = 'GDL/1950'
         filter_config=np_config
@@ -22,7 +22,7 @@ def test_impresso_iter_bucket():
 
 
 def test_s3_iter_bucket():
-    b = get_bucket("canonical-rebuilt-lux", create=False)
+    b = get_bucket("canonical-rebuilt", create=False)
     keys = s3_iter_bucket(b.name, prefix='GDL', suffix=".bz2")
     assert keys is not None
     assert len(keys) > 0
@@ -30,7 +30,7 @@ def test_s3_iter_bucket():
 
 def test_s3_filter_archives():
     config = {"GDL": [1950, 1959]}
-    b = get_bucket("canonical-rebuilt-lux", create=False)
+    b = get_bucket("canonical-rebuilt", create=False)
     keys = s3_filter_archives(b.name, config=config)
     assert keys is not None
     assert len(keys) == 9
