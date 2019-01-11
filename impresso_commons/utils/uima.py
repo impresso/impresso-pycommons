74635305c1b7266c9573e3ef34b5721e10d7309b
mromanello <matteo.romanello@gmail.com>
2019-01-11 16:25:43 +0100

    refactored rebuilt2xmi function/test according to maud's comments in PR

--- impresso_commons/utils/uima.py
+++ impresso_commons/utils/uima.py
@@ -6,20 +6,31 @@ from pycas.cas.writer.XmiWriter import XmiWriter
 from pycas.type.cas.TypeSystemFactory import TypeSystemFactory
 
 
-def rebuilt2xmi(document, output_dir, typesystem_path):
+def rebuilt2xmi(ci, output_dir, typesystem_path):
     """
-    TODO
+    Converts a rebuilt ContentItem into Apache UIMA/XMI format.
+
+    The resulting file will be named after the content item's ID, adding
+    the `.xmi` extension.
+
+    :param ci: the content item to be converted
+    :type ci: `impresso_commons.classes.ContentItem`
+    :param output_dir: the path to the output directory
+    :type output_dir: str
+    :param typesystem_path: TypeSystem file containing defitions of annotation
+    layers.
+    :type typesystem_path: str
     """
     tsf = TypeSystemFactory()
     tsf = tsf.readTypeSystem(typesystem_path)
     cas = CAS(tsf)
-    cas.documentText = document.fulltext
+    cas.documentText = ci.fulltext
     cas.sofaMimeType = 'text'
     sentType = 'de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence'
 
     # create sentence-level annotations
     start_offset = 0
-    for break_offset in document.lines:
+    for break_offset in ci.lines:
         start = start_offset
         end = break_offset
         start_offset = break_offset
@@ -27,6 +38,6 @@ def rebuilt2xmi(document, output_dir, typesystem_path):
         cas.addToIndex(sntc)
 
     writer = XmiWriter()
-    outfile_path = os.path.join(output_dir, f'{document.id}.xmi')
+    outfile_path = os.path.join(output_dir, f'{ci.id}.xmi')
 
     writer.write(cas, outfile_path)
--- tests/utils/test_uima.py
+++ tests/utils/test_uima.py
@@ -1,17 +1,16 @@
 """Tests for the module `impresso_commons.utils.uima`."""
 
-from dask import bag as db
-from impresso_commons.text import RebuiltDocument
-from impresso_commons.utils.uima import rebuilt2xmi
-import pkg_resources
-import os
 import json
+
+import pkg_resources
+from dask import bag as db
+
+from impresso_commons.classes import ContentItem, ContentItemCase
+from impresso_commons.utils.s3 import IMPRESSO_STORAGEOPT as impresso_s3
+from impresso_commons.utils.uima import rebuilt2xmi
 
 
 def test_rebuilt2xmi():
-    my_access = os.environ['SE_ACCESS_KEY']
-    my_secret = os.environ['SE_SECRET_KEY']
-    host = 'https://os.zhdk.cloud.switch.ch'
 
     output_dir = pkg_resources.resource_filename(
         'impresso_commons',
@@ -24,16 +23,12 @@ def test_rebuilt2xmi():
 
     b = db.read_text(
         's3://canonical-rebuilt/GDL/GDL-1910.jsonl.bz2',
-        # replace with `impresso_s3`
-        storage_options={
-            'client_kwargs': {'endpoint_url': host},
-            'key': my_access, 'secret': my_secret
-         }
+        storage_options=impresso_s3
     )
     # .map(lambda x: json.loads(x))
 
     texts = b.compute()
     text = json.loads(texts[10])
-    doc = RebuiltDocument.from_json(data=text)
+    doc = ContentItem.from_json(data=text, case=ContentItemCase.FULL)
     print(doc)
     rebuilt2xmi(doc, output_dir, typesystem)
