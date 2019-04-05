b93d4cce044d0c647edee63644a2bbaea0b1db19
Matteo Romanello <matteo.romanello@gmail.com>
2019-04-05 16:19:10 +0200

    added test for rebuilding IMP

--- tests/test_rebuilder.py
+++ tests/test_rebuilder.py
@@ -1,5 +1,6 @@
 from impresso_commons.path.path_s3 import impresso_iter_bucket
 from impresso_commons.text.rebuilder import rebuild_issues, compress
+from impresso_commons.text.helpers import read_s3_issues
 from dask.distributed import Client
 import pkg_resources
 import logging
@@ -73,6 +74,29 @@ def test_rebuild_GDL():
         prefix="GDL/1799/01/0",
         item_type="issue"
     )
+
+    issue_key, json_files = rebuild_issues(
+        issues=input_issues,
+        input_bucket=input_bucket_name,
+        output_dir=outp_dir,
+        dask_client=client,
+        format='solr'
+    )
+
+    result = compress(issue_key, json_files, outp_dir)
+    logger.info(result)
+    assert result is not None
+
+
+def test_rebuild_IMP():
+    input_bucket_name = "s3://original-canonical-compressed"
+    outp_dir = pkg_resources.resource_filename(
+        'impresso_commons',
+        'data/rebuilt'
+    )
+
+    input_issues = read_s3_issues("IMP", "1994", input_bucket_name)
+    print(f'{len(input_issues)} issues to rebuild')
 
     issue_key, json_files = rebuild_issues(
         issues=input_issues,
