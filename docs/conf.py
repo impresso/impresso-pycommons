c8ee8fe0a4e2c2e811ac33e2e908fc3296842fec
piconti <pauline.conti@epfl.ch>
2023-12-11 17:36:40 +0100

    remove html_static_path

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,6 @@ source_suffix = ['.rst', '.md']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
 autodoc_mock_imports = [
     'jsonlines',
